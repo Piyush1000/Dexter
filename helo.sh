f89c141363541e44c8a0eae9918afe32ba6aff67
Piyush1000 <piyush.thapar500@gmail.com>
2018-06-22 07:11:50 +0000

    Add files via upload

---
+++ helo.sh
@@ -0,0 +1,51 @@
+# !bin/bash
+echo "           ***********************************************************************************************************"
+echo "             ********************************  WELCOME TO THE DEXTER******************************************"
+echo "        ************************WE ARE HERE TO HELP YOU DISPOSE A BODY!!!!!ITS FUN RIGHT.... : ) **************"
+echo "         *********************SO FIRST WE NEED TO DO SOME STANDARD INFO PROCEDURE HOPE YOU COOPERATE.....: ) *****"
+echo ""
+echo "PLEASE STATE YOUR FULL NAME"
+read name
+echo ""
+echo "PLEASE WRITE YOUR AGE  (FOR THE LEGAL AGE ISSUES NOTHING MUCH..: ) "
+read age
+
+if [ $age -lt 18 ]
+then 
+echo " SORRY BRUH YOU ARE ON YOUR OWN.. "
+else
+echo ""
+echo ""
+echo "PLEASE TYPE YOUR EMAIL IN THE NEXT LINE SOO WE CANT CONTACT YOU WITH AMAZING OFFERS LIKE A ELECTRIC CHAINSAW,CONCENTRATED HYDROCHLORIC ACID,ETC.." 
+read email
+echo ""
+echo "SO.. $name HOW DID YOU KILLED THE VICTIM ???"
+read method
+echo ""
+echo ""
+echo "...HMMMMMM..... $method is a interesting way to kill a person and dont worry we will be teaching you the art of Disposing....: ) : )"
+echo ""
+echo ""
+echo ""
+echo " NOW IN THE NEXT LINE TELL US THE APROXX HEIGHT , WEIGHT , BUILT OF THE BODY IN ORDER AS ASKED... *_*	"
+read measure
+
+echo "So..THE SPECIFICATIONS SEEMS FINE ... "
+echo ""
+echo ""
+echo ""
+echo "NOW DO US A FAVOUR JUST SELECT SOME OPTIONS"
+echo ""
+echo ""
+echo " 1. I OWN A BATHTUB"
+echo " 2. I AM A TRAINED CHEMIST"
+echo " 3. I OWN A AXE/CHAINSAW"
+echo " 4. I LIVE NEAR A WATERBODY (NOT A LAKE ONLY RUNNING WATERBODIES)"
+echo " 5. I LIVE NEAR A GRAVEYARD"
+echo " 6. I WORK AT A DISPOSABLE GROUND"
+echo ""
+read options
+echo ""
+
+fi
+
